fb925be467c456b7fb783b7fa71d9c36b2598db1
MustansarFarooq <mushoo2442@outlook.com>
2025-07-31 18:54:15 -0400

    before i screw something up with player class

--- src/main.cpp
+++ src/main.cpp
@@ -2,24 +2,27 @@
 #include <iostream>
 #include <math.h>
 #include <string>
+#include "player.h"
 using namespace sf;
 using namespace std;
+
 const Font font("font.ttf");
-const float PI = 3.14159265358979323846;
 const int screenWidth = 800;
 const int screenHeight = 600;
 const int mapHeight = 16;
 const int mapWidth = 16;
 const float playerSpeed = 5.0f;
 const float rotationSpeed = 1.5f;
-const float toDegrees = 180 / PI;
 
 const float depth = 16.0f;
 Vector2f playerPosition(8.5f, 8.5f);
 Vector2f playerDirection(0.f, 1.f);
 Vector2f cameraPlane(0.66f, 0.f);
-float FOV = PI / 2.f;
 Vector2f playerVelocity(0.f, 0.f);
+
+//Player player(8.5f,8.5f);
+
+
 
 float deltaTime;
 Clock deltaClock;
@@ -43,8 +46,7 @@ const int map[mapHeight * mapWidth] = {
         1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
 };
 
-int main()
-{
+int main() {
     RenderWindow window(VideoMode({screenWidth, screenHeight}), "Raycaster");
     VertexArray line(PrimitiveType::Lines, 2);
     float fpsTimer = 0;
@@ -53,29 +55,29 @@ int main()
 
     Text tFPS(font, '0', 20);
     tFPS.setPosition({10, 10});
-    while (window.isOpen())
-    {
+    while (window.isOpen()) {
         playerVelocity = Vector2f(0.f, 0.f);
         deltaTime = deltaClock.restart().asSeconds();
         fpsTimer += deltaTime;
 
-        while (const optional event = window.pollEvent())
-        {
-            if (event->is<Event::Closed>())
-            {
+        while (const optional event = window.pollEvent()) {
+            if (event->is<Event::Closed>()) {
                 window.close();
             }
         }
 
-        // counterclockwise rotation matrix:
+
+
+        //counterclockwise rotation matrix:
 
         // $$R = \begin{bmatrix}\ \cos{\theta}\ -\sin{\theta}\\ \sin{\theta} \ \cos{\theta} \\\end{bmatrix}$$
 
         // and then to do it clockwise, make theta negative?
-        // vector multiplication is weird. i jus got this code from chatgpt but yh fr
+        // vector multiplication is weird. i jus got this code from chatgpt but yh fr 
 
-        if (Keyboard::isKeyPressed(Keyboard::Key::Left))
-        {
+
+
+        if (Keyboard::isKeyPressed(Keyboard::Key::Left)) {
             float cosTheta = cos(rotationSpeed * deltaTime);
             float sinTheta = sin(rotationSpeed * deltaTime);
             float oldDirX = playerDirection.x;
@@ -84,51 +86,49 @@ int main()
             playerDirection.y = oldDirX * sinTheta + playerDirection.y * cosTheta;
 
             float oldPlaneX = cameraPlane.x;
-            cameraPlane.x = cameraPlane.x * cosTheta - cameraPlane.y * sinTheta;
-            cameraPlane.y = oldPlaneX * sinTheta + cameraPlane.y * cosTheta;
+            cameraPlane.x = cameraPlane.x*cosTheta - cameraPlane.y * sinTheta;
+            cameraPlane.y = oldPlaneX*sinTheta + cameraPlane.y *cosTheta;
+
         }
-        if (Keyboard::isKeyPressed(Keyboard::Key::Right))
-        {
-            float cosTheta = cos(-rotationSpeed * deltaTime);
-            float sinTheta = sin(-rotationSpeed * deltaTime);
+        if (Keyboard::isKeyPressed(Keyboard::Key::Right)) {
+            float cosTheta = cos(-rotationSpeed  * deltaTime);
+            float sinTheta = sin(-rotationSpeed  * deltaTime);
             float oldDirX = playerDirection.x;
 
             playerDirection.x = playerDirection.x * cosTheta - playerDirection.y * sinTheta;
             playerDirection.y = oldDirX * sinTheta + playerDirection.y * cosTheta;
 
             float oldPlaneX = cameraPlane.x;
-            cameraPlane.x = cameraPlane.x * cosTheta - cameraPlane.y * sinTheta;
-            cameraPlane.y = oldPlaneX * sinTheta + cameraPlane.y * cosTheta;
-        }
+            cameraPlane.x = cameraPlane.x*cosTheta - cameraPlane.y * sinTheta;
+            cameraPlane.y = oldPlaneX*sinTheta + cameraPlane.y *cosTheta;
 
-        if (Keyboard::isKeyPressed(Keyboard::Key::W))
-        {
-            playerVelocity = playerDirection * playerSpeed * deltaTime;
         }
-        if (Keyboard::isKeyPressed(Keyboard::Key::S))
-        {
+        
+        
+        if(Keyboard::isKeyPressed(Keyboard::Key::W)) {
+            playerVelocity =playerDirection*playerSpeed*deltaTime;
+        }
+        if (Keyboard::isKeyPressed(Keyboard::Key::S)) {
             playerVelocity = -playerDirection * playerSpeed * deltaTime;
         }
-        if (Keyboard::isKeyPressed(Keyboard::Key::A))
-        {
+        if (Keyboard::isKeyPressed(Keyboard::Key::A)) {
             Vector2f leftStrafe = Vector2f(-playerDirection.y, playerDirection.x);
             playerVelocity = leftStrafe * playerSpeed * deltaTime;
+
         }
-        if (Keyboard::isKeyPressed(Keyboard::Key::D))
-        {
+        if (Keyboard::isKeyPressed(Keyboard::Key::D)) {
             Vector2f rightStrafe = Vector2f(playerDirection.y, -playerDirection.x);
             playerVelocity = rightStrafe * playerSpeed * deltaTime;
         }
-
+        
         Vector2f projectedPosition(playerPosition.x + playerVelocity.x, playerPosition.y + playerVelocity.y);
-        if (map[(int)projectedPosition.y * mapWidth + (int)projectedPosition.x] != 1)
-        {
+        if (map[(int)projectedPosition.y * mapWidth + (int)projectedPosition.x] != 1) {
             playerPosition += playerVelocity;
-        }
+        }   
+       
         window.clear();
 
-        for (int x = 0; x < screenWidth; x++)
-        {
+        for (int x = 0; x < screenWidth; x++) {
 
             //$$ Ray_{Angle} = (Player_{angle} - \frac{FOV}{2})+\frac{x}{screenWidth} \times FOV$$
 
@@ -138,39 +138,34 @@ int main()
 
             // offset and add the actual angle as according to what column of the console we are on.
 
-            float cameraX = 2 * float(x) / float(screenWidth) - 1; // range from -1 to 1? idk
+            float cameraX = 2*float(x)/float(screenWidth) -1; //range from -1 to 1? idk
             Vector2f rayDir = playerDirection + cameraPlane * cameraX;
+            
 
             // $$\hat{V} = \begin{bmatrix}\sin{\theta}\\\cos{\theta}\\\end{bmatrix}$$
 
             Vector2f rayDirection = rayDir;
             Vector2f rayStart = playerPosition;
-            Vector2f unitStepSize(sqrt(1 + (rayDirection.y / rayDirection.x) * (rayDirection.y / rayDirection.x)), sqrt(1 + (rayDirection.x / rayDirection.y) * (rayDirection.x / rayDirection.y)));
+            Vector2f unitStepSize(sqrt(1+(rayDirection.y/rayDirection.x)*(rayDirection.y/rayDirection.x)),sqrt(1+(rayDirection.x/rayDirection.y)*(rayDirection.x/rayDirection.y)));
+            
+            Vector2i currentTile(int(rayStart.x),int(rayStart.y));
+            Vector2f rayLength; //the x value is the ray lenght when we add the step.x, and the y value is when we add step.y
+            Vector2f step;//negative or positive depending on direction
 
-            Vector2i currentTile(int(rayStart.x), int(rayStart.y));
-            Vector2f rayLength; // the x value is the ray lenght when we add the step.x, and the y value is when we add step.y
-            Vector2f step;      // negative or positive depending on direction
-
-            if (rayDirection.x < 0)
-            {
+            if (rayDirection.x < 0) {
                 step.x = -1;
                 rayLength.x = (rayStart.x - currentTile.x) * unitStepSize.x;
-            }
-            else
-            {
-                step.x = 1;
-                rayLength.x = ((currentTile.x + 1) - rayStart.x) * unitStepSize.x;
+            } else {
+                step.x = 1; 
+                rayLength.x = ((currentTile.x+1)-rayStart.x) * unitStepSize.x;
             }
 
-            if (rayDirection.y < 0)
-            {
+            if (rayDirection.y < 0) {
                 step.y = -1;
                 rayLength.y = (rayStart.y - currentTile.y) * unitStepSize.y;
-            }
-            else
-            {
-                step.y = 1;
-                rayLength.y = ((currentTile.y + 1) - rayStart.y) * unitStepSize.y;
+            } else {
+                step.y = 1; 
+                rayLength.y = ((currentTile.y+1)-rayStart.y) * unitStepSize.y;
             }
             bool hitWall = false;
             float distanceToWall = 0.0f;
@@ -191,13 +186,14 @@ int main()
 
                 if (currentTile.x >= 0 && currentTile.x <= mapWidth && currentTile.y >= 0 && currentTile.y <= mapHeight)
                 {
-                    if (map[int(currentTile.y) * mapWidth + int(currentTile.x)] == 1)
+                    if(map[int(currentTile.y)*mapWidth+int(currentTile.x)] == 1)
                     {
-                        hitWall = true;
+                            hitWall = true;
+                            
                     }
                 }
             }
-
+            
             float ceiling = (float)(screenHeight / 2.0f) - (screenHeight / (float)distanceToWall);
             float floor = screenHeight - ceiling;
             uint8_t shade = static_cast<uint8_t>(255 - std::min(distanceToWall * 16, 255.0f));
@@ -209,8 +205,7 @@ int main()
         }
 
         // updating the fps once per second
-        if (fpsTimer >= 1)
-        {
+        if (fpsTimer >= 1) {
             float fps = 1 / deltaTime;
             fps = trunc(fps);
             sFPS.str("");
@@ -219,7 +214,9 @@ int main()
             fpsTimer = 0;
         }
         window.draw(tFPS);
+        
 
+        
         window.display();
     }
 }
