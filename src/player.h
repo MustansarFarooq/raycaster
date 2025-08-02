29385b10108a200ce940e45e2b3931e9e5321a70
MustansarFarooq <mushoo2442@outlook.com>
2025-08-01 23:55:12 -0400

    Player Class

--- src/player.h
+++ src/player.h
@@ -2,22 +2,22 @@
 #include <SFML/Graphics.hpp>
 
 class Player
-{
-
-    sf::Vector2f position;
-    sf::Vector2f direction{0.f,1.f};
+{   
     sf::Vector2f velocity{0.f,0.f};
+    sf::Vector2f projectedPosition;
     const float movementSpeed = 5.f;
     const float rotationSpeed = 1.5f;
 
 
 public:
+    sf::Vector2f direction{0.f,1.f};    
+    sf::Vector2f position;
     Player(float x, float y);
 
-    sf::Vector2f movement(float deltaTime);
+    void movement(float deltaTime);
     void cameraControls(float deltaTime, sf::Vector2f& cameraPlane);
     void draw(sf::RenderWindow &window);
 
-    sf::Vector2f getPosition() const;
-    sf::Vector2f getDirection() const;
+    sf::Vector2f getProjectedPosition();
+    sf::Vector2f getDirection();
 };
